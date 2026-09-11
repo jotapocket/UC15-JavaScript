d803b76845e75e1b216b37d3c660afad3d22ae8b
João Pedro <noobdevjoao@gmail.com>
2026-09-11 17:41:11 -0300

    Add files via upload

--- UC15 - Funções/script.js
+++ UC15 - Funções/script.js
@@ -1,4 +1,4 @@
-console.log("Olá, Luiz! Seja bem-vindo!");
+/*console.log("Olá, Luiz! Seja bem-vindo!");
 console.log("Olá, Gustavo! Seja bem-vindo!");
 console.log("Olá, Cecília! Seja bem-vinda!");
 
@@ -43,3 +43,44 @@ function calcularmédia(nota1, nota2) {
 
 calcularmédia(8, 9);
 calcularmédia(5, 6);
+*/
+
+// 11/09/2026
+
+/* function calcularMedia(nota1, nota2) {
+    return (nota1 + nota2) / 2;
+}  
+
+let nome = prompt("Digite seu nome:");
+let nota1 = Number(prompt("Digite a primeira nota:"));
+let nota2 = Number(prompt("Digite a segunda nota:"));
+
+let media1 = calcularMedia(nota1, nota2);
+console.log(`Olá, ${nome}! Sua média é: ${media1}`);
+
+if (media1 >= 6) {
+    console.log("Parabéns! Você foi aprovado(a)!");
+} else {
+    console.log("Infelizmente, você não foi aprovado(a).");
+}
+
+calcularMedia(nota1, nota2); */
+
+// Atv
+
+function calcularViagem(passagem, hospedagem, alimentacao, passeios) {
+    return passagem + hospedagem + alimentacao + passeios;
+}
+
+let passagem = Number(prompt("Digite o valor da passagem:"));
+let hospedagem = Number(prompt("Digite o valor da hospedagem:"));
+let alimentacao = Number(prompt("Digite o valor da alimentação:"));
+let passeios = Number(prompt("Digite o valor dos passeios:"));
+let totalViagem = calcularViagem(passagem, hospedagem, alimentacao, passeios);
+console.log(`O valor total da viagem é: ${totalViagem}`);
+
+if (totalViagem > 2000) {
+    console.log("O valor total da viagem não está no orçamento. Considere reduzir os gastos.");
+} else {
+    console.log("O valor total da viagem está dentro do orçamento.");
+}
